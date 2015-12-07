4ae749cd9bbe8ef8b56345dc5aeb76c17eb558ea
Chris Sherwood <csherwood@usgs.gov>
2015-12-07 15:32:08 -0500

    pfit added

--- floc_proc.m
+++ floc_proc.m
@@ -1,4 +1,4 @@
- % floc_proc.m - Script to read and plot ROMS .his files
+% floc_proc.m - Script to read and plot ROMS .his files
 clear
 cas = 78
 fn = 0;
@@ -75,6 +75,13 @@ end
 dzw = diff(z_w,1,1);
 tz = repmat(ocean_time',50,1);
 elev = h+z_w(:,1);
+%% Read in stresses
+rho0 = ncread(url,'rho0');
+bustrc=squeeze(ncread(url,'bustrc',[i j 1],[1 1 Inf]));
+tauc = (bustrc.^2+squeeze(ncread(url,'bvstrc',[i j 1],[1 1 Inf])).^2).^(.5);
+taucwm = (squeeze(ncread(url,'bustrcwmax',[i j 1],[1 1 Inf])).^2+squeeze(ncread(url,'bvstrcwmax',[i j 1],[1 1 Inf])).^2).^(.5);
+ustrc = sqrt(tauc./rho0);
+ustrcw = sqrt(taucwm./rho0);
 %% read 15 mud classes, one cell only, for 1D results
 nt = length(ocean_time);
 nz = length(s_rho);
@@ -177,7 +184,7 @@ r_lisstc=zeros(nz,nt);
 for jj=1:nt
     for ii=1:nz
         mv = [c_ac9_i.*squeeze(m(:,ii,jj)); c_ac9_is.*squeeze(snd(:,ii,jj))];
-        r_ac9c(ii,jj)=sum(mv); 
+        r_ac9c(ii,jj)=sum(mv);
         mv = [c_lisst_i.*squeeze(m(:,ii,jj)); c_lisst_is.*squeeze(snd(:,ii,jj))];
         r_lisstc(ii,jj)=sum(mv);
     end
@@ -205,7 +212,7 @@ fdiamall = squeeze((sum(repmat(fdiam(1:NCS),1,nz,nt).*m)...
 %% fdiamlisst - same calc, but omit diameters outside LISST range
 ml = m(1:NCS,:,:);
 sndl = snd;
-toobig = find(fdiam(1:NCS)>250e-6); 
+toobig = find(fdiam(1:NCS)>250e-6);
 toosmall = find(fdiam(1:NCS)<4e-6);
 ml(toobig,:,:)=0;
 ml(toosmall,:,:)=0;
@@ -215,4 +222,81 @@ sndl(toobigs,:,:)=0;
 sndl(toosmalls,:,:)=0;
 fdiamlisst = squeeze((sum(repmat(fdiam(1:NCS),1,nz,nt).*ml)...
     +                 sum(repmat(fdiam(NNN+NCS+1:NCS+NNN+NND),1,nz,nt).*sndl))) ...
-     ./squeeze(sum(sndl)+sum(ml));
+    ./squeeze(sum(sndl)+sum(ml));
+%% calculate apparent settling velocity
+s2d = 1. /(3600.*24.);
+ic = 1;
+iz = 2:10;
+za = 0.1;
+z=elev(iz);
+fprintf(1,'Elevations for pfit:');
+disp(z)
+clear pfa pfs pfm pfv pfac9 pfalisst
+for ii=1:nt
+    figure(1); clf
+    t = s2d*ocean_time(ii);
+    c = squeeze(muds(iz,ii)+snds(iz,ii));
+    hold on
+    pf = pfit( c, z, 1, za);
+    c = squeeze(snds(iz,ii));
+    pfs = pfit( c, z, 1, za);
+    c = squeeze(muds(iz,ii));
+    pfm = pfit( c, z, 1, za);
+    
+    c = squeeze(vcomb25(iz,ii));
+    pfvcombc = pfit( c, z, 0, za);
+    c = squeeze(r_ac9c(iz,ii));
+    pfac9c = pfit( c, z, 0, za);
+    c = squeeze(r_lisstc(iz,ii));
+    pfalisstc = pfit( c, z, 0, za);
+    
+    ylim( [.1 2.5] );
+    xlim( [.01 1] );
+    ylabel('Elevation (mab)')
+    xlabel('Attenuation ( m^{-1} )');
+    
+    ts = sprintf('%6.2f\nN=%d\nCa=%7.2f\np=% 5.2f\nr^2=%06.4f\n',...
+        t,pf.N,pf.Ca,-pf.p,pf.r2);
+    text(.2,1.5,ts)
+    shg
+    pause(.2)
+    pfa(ic)=pf;
+    pfsa(ic)=pfs;
+    pfma(ic)=pfm;
+    pfv(ic)=pfvcombc;
+    pfac9(ic)=pfac9c;
+    pfalisst(ic)=pfalisstc;
+    ic = ic+1;
+end
+%% plot the pfit results
+figure(2); clf
+subplot(411)
+plot(s2d*ocean_time,zeros(size(ocean_time)),'--k')
+hold on
+h1=plot(s2d*ocean_time,sign(bustrc).*ustrc,'linewidth',2,'color',[.1 .1 .4])
+h2=plot(s2d*ocean_time,ustrcw,'linewidth',2,'color',[.3 .3 .6])
+
+subplot(412)
+h1=plot(s2d*ocean_time,-1e3*[pfa.p]'.*(0.41*ustrc),'linewidth',2)
+hold on
+h2=plot(s2d*ocean_time,-1e3*[pfsa.p]'.*(0.41*ustrc),'linewidth',2)
+h3=plot(s2d*ocean_time,-1e3*[pfma.p]'.*(0.41*ustrc),'linewidth',2)
+h4=plot(s2d*ocean_time,-1e3*[pfv.p]'.*(0.41*ustrc),'linewidth',2)
+h5=plot(s2d*ocean_time,-1e3*[pfac9.p]'.*(0.41*ustrc),'linewidth',2)
+h6=plot(s2d*ocean_time,-1e3*[pfalisst.p]'.*(0.41*ustrc),'linewidth',2)
+legend([h1;h2;h3;h4;h5;h6],'All','Sand','Flocs','2.5 MHz','ac9','LISST')
+
+subplot(413)
+h1=plot(s2d*ocean_time,[pfa.p],'linewidth',2)
+hold on
+h2=plot(s2d*ocean_time,[pfv.p],'linewidth',2)
+h3=plot(s2d*ocean_time,[pfac9.p],'linewidth',2)
+h4=plot(s2d*ocean_time,[pfalisst.p],'linewidth',2)
+legend([h1;h2;h3;h4],'Mass','2.5 MHz','ac9','LISST')
+subplot(414)
+h1=plot(s2d*ocean_time,[pfa.r2],'linewidth',2)
+hold on
+h2=plot(s2d*ocean_time,[pfv.r2],'linewidth',2)
+h3=plot(s2d*ocean_time,[pfac9.r2],'linewidth',2)
+h4=plot(s2d*ocean_time,[pfalisst.r2],'linewidth',2)
+legend([h1;h2;h3;h4],'Mass','2.5 MHz','ac9','LISST')
